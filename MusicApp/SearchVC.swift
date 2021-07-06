d09eb00a366d94864912903ed54976e5fc1bb294
Кирилл Федоров <ikirillfedorov@gmail.com>
2021-07-06 05:56:06 +0300

    add search controller

--- MusicApp/SearchVC.swift
+++ MusicApp/SearchVC.swift
@@ -9,8 +9,50 @@ import UIKit
 
 final class SearchVC: UITableViewController {
 	
+	let array = [
+		TrackModel(name: "Test1", artistName: "Test2"),
+		TrackModel(name: "Test1", artistName: "Test2"),
+		TrackModel(name: "Test1", artistName: "Test2"),
+		TrackModel(name: "Test1", artistName: "Test2"),
+		TrackModel(name: "Test1", artistName: "Test2")
+	]
+	
+	let searchController = UISearchController(searchResultsController: nil)
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		view.backgroundColor = .orange
+		searchController.searchBar.delegate = self
+		registerCells()
+		setupSearchBar()
+	}
+	
+	private func registerCells() {
+		tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+	}
+	
+	private func setupSearchBar() {
+		navigationItem.searchController = searchController
+		navigationItem.hidesSearchBarWhenScrolling = false
+	}
+	
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return array.count
+	}
+	
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+		let model = array[indexPath.row]
+		cell.imageView?.image = UIImage(named: "search_ic")
+		cell.textLabel?.text = model.name
+		cell.detailTextLabel?.text = model.name
+		return cell
+	}
+}
+
+// MARK: UISearchBarDelegate
+
+extension SearchVC: UISearchBarDelegate {
+	func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+		print(searchText)
 	}
 }
